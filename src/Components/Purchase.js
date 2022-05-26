5ea9a72d8faef0a3e27cc55835b301bdb4da9ee2
Ai Shadhin <aishadhin.ac@gmail.com>
2022-05-26 13:28:26 +0600

    order place

--- src/Components/Purchase.js
+++ src/Components/Purchase.js
@@ -3,26 +3,30 @@ import { useParams } from "react-router-dom";
 
 const Purchase = () => {
   const [product, setProduct] = useState([]);
-  const [quantity, setQuantity] = useState(10);
 
   useEffect(() => {
     fetch(`http://localhost:5000/product/${id}`)
       .then((res) => res.json())
       .then((data) => setProduct(data));
   }, []);
-
-  const qtyChange = (event) => {
-    console.log(event.target.value)
-  };
-
-  const handleQty = () => {
-    if (quantity < product.minQty) {
-      alert("kam hobo na");
-    } else {
-    }
-  };
-
   const { id } = useParams();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const availableQty = event.target.availableQty.value;
+    const minOrder = event.target.minOrder.value;
+    const price = event.target.price.value;
+    const order = event.target.order.value;
+
+    if (order < product.minQty || order > product.avilableQty) {
+      alert("Please order between minimum and available number of products");
+      return
+    }
+
+    const orderDetails = { availableQty, minOrder, price, order };
+    console.log(orderDetails);
+  };
+
   return (
     <div className="p-6 w-[70%] mx-auto">
       <h2 className="text-3xl font-bold text-center uppercase mb-6">
@@ -31,14 +35,70 @@ const Purchase = () => {
       <div className="flex justify-around my-12">
         <img src={product.img} alt="" />
         <div>
-          <h2 className="text-2xl font-bold">{product.name}</h2>
-          <p>Available Quantity: {product.avilableQty}</p>
-          <p>Minimum Order Quantity: {product.minQty}</p>
-          <p>Price per peace: ${product.priceUnit}</p>
-          <div className="mt-6 flex align-center">
-             
-          </div>
-          <p>Total price: </p>
+          <form onSubmit={handleSubmit}>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Available Quantity</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                name="availableQty"
+                value={product.avilableQty}
+                disabled
+                class="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Minimum Order Quantity</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                name="minOrder"
+                value={product.minQty}
+                disabled
+                class="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Price per peace in $</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                value={product.priceUnit}
+                name="price"
+                disabled
+                class="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Your order quantity</span>
+              </label>
+              <input
+                type="text"
+                name="order"
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Total Price</span>
+              </label>
+            </div>
+            <div class="form-control w-full max-w-xs">
+              <input
+                type="submit"
+                class="input mt-3 input-bordered w-full max-w-xs bg-primary text-white"
+                value="Place Order"
+              />
+            </div>
+          </form>
         </div>
       </div>
     </div>
